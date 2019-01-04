638dcc447c62d8e1c716dedf89cb77567aea5393
Ibrahim Olufowora <ioolofowora@sidmach.com>
2019-01-04 21:33:55 +0100

    Add route to get all meetups

--- api/routes/meetup.js
+++ api/routes/meetup.js
@@ -3,10 +3,11 @@ import meetupController from '../controllers/meetup';
 import meetupvalidator from '../validators/meetupvalidator';
 
 const {
-    createMeetup} = meetupController;
+    createMeetup, getMeetups} = meetupController;
 
 const router = express.Router();
 
 router.post('/', meetupvalidator, createMeetup);
+router.get('/', getMeetups);
 
 export default router;
