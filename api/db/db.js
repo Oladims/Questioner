7e3b45a394f53329b30610092c4eaf3b7f69f057
Ibrahim Olufowora <ioolofowora@sidmach.com>
2019-01-04 21:26:13 +0100

    Create dummy data for meetup

---
+++ api/db/db.js
@@ -0,0 +1,25 @@
+
+export let meetupRecords = [{
+    "id": 1,
+    "topic": "Concatenate",
+    "location": "Lagos",
+    "happeningOn": "2020-04-23T18:25:43.511Z",
+    "tags": [],
+    "createdOn": "2019-01-03T07:13:00.135Z"
+},
+{
+    "id": 2,
+    "topic": "GDG",
+    "location": "Lagos",
+    "happeningOn": "2018-04-23T18:25:43.511Z",
+    "tags": [],
+    "createdOn": "2019-01-03T07:13:00.135Z"
+},
+{
+    "id": 3,
+    "topic": "Adobe",
+    "location": "Lagos",
+    "happeningOn": "2019-04-23T18:25:43.511Z",
+    "tags": [],
+    "createdOn": "2019-01-03T07:16:09.975Z"
+}];
