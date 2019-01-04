c3ea800ac980d7ed297717562045ebf37d389dd7
Ibrahim Olufowora <ioolofowora@sidmach.com>
2019-01-04 21:42:58 +0100

    Add function and route to get all upcoming meetup

--- api/routes/meetup.js
+++ api/routes/meetup.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import meetupController from '../controllers/meetup';
 import meetupvalidator from '../validators/meetupvalidator';
+import idValidator from '../validators/idvalidator';
 
 const {
     createMeetup, getMeetups} = meetupController;
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post('/', meetupvalidator, createMeetup);
 router.get('/', getMeetups);
 router.get('/:id', idValidator, getMeetup);
+router.get('/upcoming', getUpcomingMeetups);
 
 export default router;
