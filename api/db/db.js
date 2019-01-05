5544967c40987b353d20832ea654408464858d80
Ibrahim Olufowora <ioolofowora@sidmach.com>
2019-01-05 13:45:42 +0100

    Edit question and meetup records

--- api/db/db.js
+++ api/db/db.js
@@ -1,4 +1,3 @@
-
 export let meetupRecords = [{
     "id": 1,
     "topic": "Concatenate",
@@ -6,24 +5,21 @@ export let meetupRecords = [{
     "happeningOn": "2020-04-23T18:25:43.511Z",
     "tags": [],
     "createdOn": "2019-01-03T07:13:00.135Z"
-},
-{
+  },
+  {
     "id": 2,
     "topic": "GDG",
     "location": "Lagos",
     "happeningOn": "2018-04-23T18:25:43.511Z",
     "tags": [],
     "createdOn": "2019-01-03T07:13:00.135Z"
-},
-{
-    "id": 3,
+  },
+  {
     "topic": "Adobe",
     "location": "Lagos",
     "happeningOn": "2019-04-23T18:25:43.511Z",
-    "tags": [],
-    "createdOn": "2019-01-03T07:16:09.975Z"
-}];
-export let questionRecords = [{
+  }];
+  export let questionRecords = [{
     "id": 1,
     "meetup": 1,
     "createdOn": '2020-01-03T07:13:00.135Z',
@@ -31,8 +27,8 @@ export let questionRecords = [{
     "body": 'What time wil the event beginWhat time wil the event begin.',
     "votes": 1,
     "createdBy": 1,
-},
-{
+  },
+  {
     "id": 2,
     "meetup": 1,
     "createdOn": '2020-01-03T07:13:00.135Z',
@@ -40,4 +36,5 @@ export let questionRecords = [{
     "body": 'What time wil the event beginWhat time wil the event begin.',
     "votes": 1,
     "createdBy": 1,
-}];
+  }];
+  
