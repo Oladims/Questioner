bac25992a5f2d6a5cb47352afa2896833d1a9997
Ibrahim Olufowora <ioolofowora@sidmach.com>
2019-01-16 10:46:56 +0100

    [Feature #163179533 Create user signup  route and controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,19 +1,48 @@
-import moment from 'moment';
 import db from '../database';
+import auth from '../validators/auth';
+import responses from '../validators/responses';
 
-export default class meetupController {
-  static createUser(req, res, next) {
-    const user = req.body;
-    const {
-      id, topic, location, happeningOn, tags,
-    } = user;
-    const queryString = 'INSERT INTO meetups (location, topic, happeningOn, tags) VALUES ($1, $2, $3, $4)';
-    const params = [location, topic, happeningOn, Array[tags]];
-    db.query(queryString, params, (err, res) => {
-      res.status(201).send({
-        status: 201,
-        message: 'Your meetup has been created successfully.',
-        data: [res.rows[0]],
+export default class userController {
+  static signup(req, res) {
+    const { error } = auth.validateUsers(req.body);
+    if (error) {
+      return res.status(400).json({
+        status: 400,
+        error: error.details[0].message,
+      });
+    }
+    console.log(req.body);
+    const hashedPassword = auth.hashPassword(req.body.password);
+    return db.query(`SELECT email FROM users WHERE email = '${req.body.email}'`, (err, result) => {
+      if (err) {
+        console.log(err);
+        return responses.errorProcessing(req, res);
+      }
+      if (result.rowCount > 0) {
+        return responses.errorAccountExist(req, res);
+      }
+      const {
+        firstname, lastname, email, phonenumber, username,
+      } = req.body;
+      const queryString = 'INSERT INTO users (firstname, lastname, email, phonenumber, password, username ) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+      const params = [firstname, lastname, email, phonenumber, hashedPassword, username];
+      return db.query(queryString, params, (err, result) => {
+        console.log(err);
+        if (err) {
+          return res.status(500).json({
+            status: 500,
+            error: err,
+          });
+        }
+        const user = result.rows[0];
+        const token = auth.generateToken(user);
+        return res.status(201).json({
+          status: 201,
+          data: [{
+            token,
+            user,
+          }],
+        });
       });
     });
   }
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,63 +1,10 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import user from '../models/user'
 import userController from '../controllers/user';
 
 const router = express.Router();
+const { signup, login } = userController;
 
-router.post('/', (req, res) => {
-  res.json({
-    message: 'post received....',
-  });
-});
-
-router.post('/login', (req, res) => {
-  const { body } = req;
-  const { username } = body;
-  const { password } = body;
-  if (username === user.username && password === user.password) {
-    // if user log in success, generate a JWT token for the user with a secret key
-    jwt.sign({ user }, 'privatekey', { expiresIn: '1h' }, (err, token) => {
-      if (err) { console.log(err); }
-      res.send(token);
-    });
-  } else {
-    console.log('ERROR: Could not log in');
-  }
-});
-
-// Check to make sure header is not undefined, if so, return Forbidden (403)
-const checkToken = (req, res, next) => {
-  const header = req.headers.authorization;
-
-  if (typeof header !== 'undefined') {
-    const bearer = header.split(' ');
-    const token = bearer[1];
-
-    req.token = token;
-    next();
-  } else {
-    // If header is undefined return Forbidden (403)
-    res.sendStatus(403);
-  }
-};
-
-router.get('/data', checkToken, (req, res) => {
-  // verify the JWT token generated for the user
-  jwt.verify(req.token, 'privatekey', (err, authorizedData) => {
-    if (err) {
-      // If error send Forbidden (403)
-      console.log('ERROR: Could not connect to the protected route');
-      res.sendStatus(403);
-    } else {
-      // If token is successfully verified, we can send the autorized data
-      res.json({
-        message: 'Successful log in',
-        authorizedData,
-      });
-      console.log('SUCCESS: Connected to protected route');
-    }
-  });
-});
+router.post('/signup', signup);
+// router.post('/login', login);
 
 export default router;
