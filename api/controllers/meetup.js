705ea9c127d8daf5513f4948f3404892e37e6722
Ibrahim Olufowora <ioolofowora@sidmach.com>
2019-01-22 13:05:30 +0100

    [Feature] update and refactor responses

--- api/controllers/meetup.js
+++ api/controllers/meetup.js
@@ -7,10 +7,7 @@ export default class meetupController {
   static createMeetup(req, res) {
     const { error } = auth.validateMeetups(req.body);
     if (error) {
-      return res.status(400).json({
-        status: 401,
-        error: error.details[0].message,
-      });
+      responses.validationError(error, req, res);
     }
     const {
       topic, location, happeningOn,
@@ -20,10 +17,7 @@ export default class meetupController {
     const params = [topic, location, happeningOn, createdOn];
     return db.query(queryString, params, (err, result) => {
       if (err) {
-        return res.status(500).json({
-          status: 500,
-          error: err,
-        });
+        responses.errorProcessing(err, req, res);
       }
       const meetup = result.rows[0];
       return res.status(201).json({
@@ -39,10 +33,7 @@ export default class meetupController {
     const queryString = 'SELECT * FROM meetup';
     return db.query(queryString, [], (err, result) => {
       if (err) {
-        return res.status(500).json({
-          status: 500,
-          error: err,
-        });
+        responses.errorProcessing(err, req, res);
       }
       const meetup = result.rows;
       return res.status(201).json({
@@ -57,20 +48,14 @@ export default class meetupController {
   static getMeetup(req, res) {
     const { error } = auth.validateId(req.params.id);
     if (error) {
-      return res.status(400).json({
-        status: 400,
-        error: error.details[0].message,
-      });
+      responses.validationError(error, req, res);
     }
     const queryString = 'SELECT * FROM meetup WHERE id = $1';
 
     const params = [req.params.id];
     return db.query(queryString, params, (err, result) => {
       if (err) {
-        return res.status(500).json({
-          status: 500,
-          error: err,
-        });
+        responses.errorProcessing(err, req, res);
       }
       const meetup = result.rows[0];
       return res.status(201).json({
@@ -86,44 +71,31 @@ export default class meetupController {
   static rsvp(req, res) {
     const { error } = auth.validateId(req.params.id);
     if (error) {
-      return res.status(400).json({
-        status: 400,
-        error: error.details[0].message,
-      });
+      responses.validationError(error, req, res);
     }
 
     const queryString = 'SELECT * FROM meetup WHERE id = $1';
     const params = [req.params.id];
     return db.query(queryString, params, (err, result) => {
       if (err) {
-        return res.status(500).json({
-          status: 500,
-          error: err,
-        });
+        responses.errorProcessing(err, req, res);
       }
 
       if (result.rowCount > 0) {
-
         const meetupTopic = result.rows[0].topic;
-        const { error2 } = auth.validateRsvp(req.body);
-        if (error2) {
-          return res.status(400).json({
-            status: 400,
-            error: error.details[0].message,
-          });
+        const { validationError } = auth.validateRsvp(req.body);
+        if (validationError) {
+          responses.validationError(req, res);
         }
         const { userId, response } = req.body;
         const queryString2 = 'INSERT INTO rsvps (meetupId, userId, response ) VALUES($1, $2, $3) RETURNING *';
         const params2 = [req.params.id, userId, response];
         return db.query(queryString2, params2, (err, result) => {
           if (err) {
-            return res.status(500).json({
-              status: 500,
-              error: err,
-            });
+            responses.errorProcessing(err, req, res);
           }
           const rsvps = result.rows[0];
-          
+
           return res.status(201).json({
             status: 201,
             data: [{
@@ -134,10 +106,7 @@ export default class meetupController {
           });
         });
       }
-      return res.status(404).json({
-        status: 404,
-        error: 'Could not find any meetup matching your request',
-      });
+      responses.alreadyExist('Meetup', req, res);
     });
   }
 }
--- api/validators/responses.js
+++ api/validators/responses.js
@@ -1,8 +1,9 @@
 const responses = {};
 
-responses.errorProcessing = (req, res) => res.status(500).json({
+responses.errorProcessing = (err, req, res) => res.status(500).json({
   status: 500,
-  error: 'Error connecting...',
+  error: 'Error processing...',
+  errorDetails: err,
 });
 
 responses.alreadyExist = (field, req, res) => res.status(403).json({
@@ -19,4 +20,10 @@ responses.incorrectPassword = (req, res) => res.status(400).json({
   status: 400,
   error: 'Incorrect Password',
 });
+
+responses.validationError = (error, req, res) => res.status(400).json({
+  status: 400,
+  error: error.details[0].message,
+});
+
 export default responses;
