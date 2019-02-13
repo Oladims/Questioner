d6b919750229b7e0c188b0c50b2fb89b299638f3
Ibrahim Olufowora <ioolofowora@sidmach.com>
2019-02-13 23:02:59 +0100

    Update user field

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import db from '../database';
 import auth from '../validators/auth';
 import responses from '../validators/responses';
@@ -9,8 +10,9 @@ export default class userController {
       return responses.validationError(error, req, res);
     }
     const {
-      firstname, lastname, othername, email, phonenumber, username, password,
+      firstname, lastname, othername, email, phonenumber, username, password, isadmin,
     } = req.body;
+    const registered = moment();
     const hashedPassword = auth.hashPassword(password);
     return db.query(`SELECT email FROM users WHERE email = '${email}'`, (err, result) => {
       if (err) {
@@ -19,8 +21,8 @@ export default class userController {
       if (result.rowCount > 0) {
         return responses.alreadyExist('Email', req, res);
       }
-      const queryString = 'INSERT INTO users (firstname, lastname, othername, email, phonenumber, password, username ) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *';
-      const params = [firstname, lastname, othername, email, phonenumber, hashedPassword, username];
+      const queryString = 'INSERT INTO users (firstname, lastname, othername, email, phonenumber, password, username, registered, isadmin) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *';
+      const params = [firstname, lastname, othername, email, phonenumber, hashedPassword, username, registered, isadmin];
       return db.query(queryString, params, (err, result) => {
         if (err) {
           return res.status(500).json({
@@ -34,7 +36,17 @@ export default class userController {
           status: 201,
           data: [{
             token,
-            user,
+            user: {
+              id: user.id,
+              firstname: user.firstname,
+              lastname: user.lastname,
+              othername: user.othername,
+              username: user.username,
+              email: user.email,
+              phonenumber: user.phonenumber,
+              registered: user.registered,
+              isadmin: user.isadmin,
+            },
           }],
         });
       });
@@ -56,8 +68,8 @@ export default class userController {
         if (auth.comparePassword(password, user.password.trim())) {
           delete user.password;
           const token = auth.generateToken(user);
-          return res.status(200).json({
-            status: 200,
+          return res.status(201).json({
+            status: 201,
             data: [{
               token,
               user,
@@ -66,7 +78,7 @@ export default class userController {
         }
         return responses.incorrectPassword(req, res);
       }
-      return responses.nonExistingAccount(req, res);
+      return responses.nonExisting('Account', req, res);
     });
   }
 }
